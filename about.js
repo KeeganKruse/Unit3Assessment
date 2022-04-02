de368a3a9814c83a0bc0d71ed3e706fbb2d4b9f1
Keegan Kruse <krusekeegan@gmail.com>
2022-04-01 23:55:25 -0600

    Initial commit

---
+++ about.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+alert('form submitted successfully');
+}
+
+let compliment = document.querySelector('img#https://i.guim.co.uk/img/media/a5fb31e646d2677f9d44104a3b26ee42955f0acc/0_170_5100_3059/master/5100.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=aa3e8bddfcc681ae45c2d642734ccdbc')
+
+let form = document.querySelector('form#contact');
+
+form.addEventListener('submit', handleSubmit);
